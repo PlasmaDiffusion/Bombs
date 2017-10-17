47600b8e659c753f137b52c93b12aed23e1e9bb3
Qing Zhao <100523799@oncampus.local>
2017-10-17 14:15:32 -0400

    Temp Test

--- Assets/Scripts/NodeGenerator.cs
+++ Assets/Scripts/NodeGenerator.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class NodeGenerator : MonoBehaviour
 {
 
-    public GameObject[] AttachedNodes;
+    private readonly bool[] AttachedNodes = { false, false, false, false };
     public GameObject NodeType;
+    public static int numNodes = 0;
+    public const int maxNodes = 10;
 
 	// Use this for initialization
 	void Start () {
@@ -15,36 +17,83 @@ public class NodeGenerator : MonoBehaviour
 
     void spawnNode(int num)
     {
-        
 
-        for (int i = 0; i < num; i++)
+        if (numNodes <= maxNodes)
         {
-            Vector3 Pos;
-            switch (Random.Range(0, 3))
-            {
-                case 0:
-                    Pos = new Vector3(gameObject.GetComponent<Renderer>().bounds.size.x,
-                        -gameObject.GetComponent<Renderer>().bounds.size.y / 2, 0);
-                    break;
-                case 1:
-                    Pos = new Vector3(0,
-                        -gameObject.GetComponent<Renderer>().bounds.size.y / 2, gameObject.GetComponent<Renderer>().bounds.size.z);
-                    break;
-                case 2:
-                    Pos = new Vector3(gameObject.GetComponent<Renderer>().bounds.size.x,
-                        -gameObject.GetComponent<Renderer>().bounds.size.y, 0);
-                    break;
-                case 3:
-                    Pos = new Vector3(0,
-                        -gameObject.GetComponent<Renderer>().bounds.size.y, 0);
-                    break;
-                default:
-                    Pos = gameObject.GetComponent<Renderer>().bounds.size;
-                    break;
-            }
+            for (int i = 0; i < num; i++)
+        {
             
-            Instantiate(NodeType, Pos, gameObject.transform.rotation);
+            
+                Vector3 Pos;
+                
+                switch (Random.Range(0, 3))
+                {
+                    case 0:
+                        if (AttachedNodes[0] == false)
+                        {
+                            AttachedNodes[0] = true;
+                            Pos = new Vector3(gameObject.GetComponent<Renderer>().bounds.size.x,
+                                -gameObject.GetComponent<Renderer>().bounds.size.y / 2, gameObject.GetComponent<Renderer>().bounds.size.z);
+                            Instantiate(NodeType, Pos, gameObject.transform.rotation);
+                            Debug.Log("generate node");
+                        }
+                        else
+                        {
+                            Debug.Log("Fuck");
+                        }
+                        break;
+                    case 1:
+                        if (AttachedNodes[1] == false)
+                        {
+                            AttachedNodes[1] = true;
+                            Pos = new Vector3(0,
+                                -gameObject.GetComponent<Renderer>().bounds.size.y / 2,
+                                gameObject.GetComponent<Renderer>().bounds.size.z);
+                            Instantiate(NodeType, Pos, gameObject.transform.rotation);
+                            Debug.Log("generate node");
+                        }
+                        else
+                        {
+                            Debug.Log("Fuck");
+                        }
+                        break;
+                    case 2:
+                        if (AttachedNodes[2] == false)
+                        {
+                            AttachedNodes[2] = true;
+                            Pos = new Vector3(gameObject.GetComponent<Renderer>().bounds.size.x,
+                                -gameObject.GetComponent<Renderer>().bounds.size.y, 0);
+                            Instantiate(NodeType, Pos, gameObject.transform.rotation);
+                            Debug.Log("generate node");
+                        }
+                        else
+                        {
+                            Debug.Log("Fuck");
+                        }
+                        break;
+                    case 3:
+                        if (AttachedNodes[3] == false)
+                        {
+                            AttachedNodes[3] = true;
+                            Pos = new Vector3(0,
+                                -gameObject.GetComponent<Renderer>().bounds.size.y, 0);
+                            Instantiate(NodeType, Pos, gameObject.transform.rotation);
+                            Debug.Log("generate node");
+                        }
+                        else
+                        {
+                            Debug.Log("Fuck");
+                        }
+                        break;
+                    default:
+                        Pos = gameObject.GetComponent<Renderer>().bounds.size;
+                        break;
+                }
+                numNodes++;
+
+            }
         }
+
     }
 	
 	// Update is called once per frame
