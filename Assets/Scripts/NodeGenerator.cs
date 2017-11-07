18e59e02fd585e9b1fedfd64fc0ce3185f9d8384
David Zhao <nyquill@ppy.sh>
2017-11-07 16:17:06 -0500

    Kill nodes over time

--- Assets/Scripts/NodeGenerator.cs
+++ Assets/Scripts/NodeGenerator.cs
@@ -12,10 +12,15 @@ public class NodeGenerator : MonoBehaviour
     public const int maxNodes = 200;
     public GameObject nodeToInitalize;
     public bool primaryNode = false;
+    public int life = 0;
+    public GameManager TimeManager;
+
+    private bool dead = false;
 
     // Use this for initialization
     void Start()
     {
+        TimeManager = GameObject.FindObjectOfType<GameManager>();
         Debug.Log("Start Called");
         if (primaryNode)
             spawnNode(8);
@@ -39,14 +44,18 @@ public class NodeGenerator : MonoBehaviour
                     case 0:
                         if (exists[0] == false)
                         {
-                            Pos = new Vector3(gameObject.GetComponent<Renderer>().bounds.size.x + gameObject.transform.position.x,
-                                -gameObject.GetComponent<Renderer>().bounds.size.y / 2, gameObject.transform.position.z);
-                            GameObject newnode = Instantiate(nodeToInitalize, Pos, gameObject.transform.rotation) as GameObject;
+                            Pos = new Vector3(
+                                gameObject.GetComponent<Renderer>().bounds.size.x + gameObject.transform.position.x,
+                                -gameObject.GetComponent<Renderer>().bounds.size.y / 2,
+                                gameObject.transform.position.z);
+                            GameObject newnode =
+                                Instantiate(nodeToInitalize, Pos, gameObject.transform.rotation) as GameObject;
                             newnode.GetComponent<NodeGenerator>().exists[1] = true;
+                            newnode.GetComponent<NodeGenerator>().life += 10;
                             AttachedNodes.Add(newnode);
                             Debug.Log("generate node");
                             exists[0] = true;
-                            
+
                         }
                         else
                         {
@@ -54,14 +63,17 @@ public class NodeGenerator : MonoBehaviour
                         }
                         break;
                     case 1:
-                        
+
                         if (exists[1] == false)
                         {
-                            Pos = new Vector3(- gameObject.GetComponent<Renderer>().bounds.size.x + gameObject.transform.position.x,
+                            Pos = new Vector3(
+                                -gameObject.GetComponent<Renderer>().bounds.size.x + gameObject.transform.position.x,
                                 -gameObject.GetComponent<Renderer>().bounds.size.y / 2,
                                 gameObject.transform.position.z);
-                            GameObject newnode = Instantiate(nodeToInitalize, Pos, gameObject.transform.rotation) as GameObject;
+                            GameObject newnode =
+                                Instantiate(nodeToInitalize, Pos, gameObject.transform.rotation) as GameObject;
                             newnode.GetComponent<NodeGenerator>().exists[0] = true;
+                            newnode.GetComponent<NodeGenerator>().life += 10;
                             AttachedNodes.Add(newnode);
                             Debug.Log("generate node");
                             exists[1] = true;
@@ -75,9 +87,12 @@ public class NodeGenerator : MonoBehaviour
                         if (exists[2] == false)
                         {
                             Pos = new Vector3(gameObject.transform.position.x,
-                                -gameObject.GetComponent<Renderer>().bounds.size.y, gameObject.GetComponent<Renderer>().bounds.size.z + gameObject.transform.position.z);
-                            GameObject newnode = Instantiate(nodeToInitalize, Pos, gameObject.transform.rotation) as GameObject;
+                                -gameObject.GetComponent<Renderer>().bounds.size.y,
+                                gameObject.GetComponent<Renderer>().bounds.size.z + gameObject.transform.position.z);
+                            GameObject newnode =
+                                Instantiate(nodeToInitalize, Pos, gameObject.transform.rotation) as GameObject;
                             newnode.GetComponent<NodeGenerator>().exists[3] = true;
+                            newnode.GetComponent<NodeGenerator>().life += 10;
                             AttachedNodes.Add(newnode);
                             Debug.Log("case 2 fire");
                             exists[2] = true;
@@ -91,9 +106,12 @@ public class NodeGenerator : MonoBehaviour
                         if (exists[3] == false)
                         {
                             Pos = new Vector3(gameObject.transform.position.x,
-                                -gameObject.GetComponent<Renderer>().bounds.size.y, -gameObject.GetComponent<Renderer>().bounds.size.z + gameObject.transform.position.z);
-                            GameObject newnode = Instantiate(nodeToInitalize, Pos, gameObject.transform.rotation) as GameObject;
+                                -gameObject.GetComponent<Renderer>().bounds.size.y,
+                                -gameObject.GetComponent<Renderer>().bounds.size.z + gameObject.transform.position.z);
+                            GameObject newnode =
+                                Instantiate(nodeToInitalize, Pos, gameObject.transform.rotation) as GameObject;
                             newnode.GetComponent<NodeGenerator>().exists[2] = true;
+                            newnode.GetComponent<NodeGenerator>().life += 10;
                             AttachedNodes.Add(newnode);
                             Debug.Log("case 3 fire");
                             exists[3] = true;
@@ -116,6 +134,11 @@ public class NodeGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (TimeManager.time <= life && !dead)
+        {
+            gameObject.AddComponent<Rigidbody>();
+            gameObject.GetComponent<Rigidbody>().useGravity = true;
+            dead = true;
+        }
     }
 }
